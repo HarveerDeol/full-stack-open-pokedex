7f73248ce43ccf70a5cc9908cff6c114a6282bb9
Harveer Deol <harveerdeol53@gmail.com>
2025-07-04 12:12:08 -0600

    added another e2e test to workflow

--- cypress/e2e/pokeapp.cy.js
+++ cypress/e2e/pokeapp.cy.js
@@ -5,3 +5,12 @@ describe('Pokedex', function() {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
+
+describe('Pokemon', function() {
+  it('ivysaur page can be opened', function() {
+    cy.visit('http://localhost:5001')
+    cy.contains('ivysaur').click()
+    cy.contains('ivysaur')
+    cy.contains('overgrow')
+  })
+})
